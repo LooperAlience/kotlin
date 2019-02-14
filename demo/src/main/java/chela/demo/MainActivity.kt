dc2d1ae51c6c406d593755c2d195337a69e6d17e
step4me <eyegochild@gmail.com>
2019-02-14 12:51:46 +0900

    Configure project build

--- demo/src/main/java/chela/demo/MainActivity.kt
+++ demo/src/main/java/chela/demo/MainActivity.kt
@@ -1,7 +1,8 @@
 package chela.demo
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
