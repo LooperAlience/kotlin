0254088524ae12c96642cd796fdcceee3dfd8cb8
summer <summer@bsidesoft.com>
2019-05-01 15:52:12 +0900

    숫자로 시작하는 문자에서 문자가 사라지고 숫자로 캐스팅되는 에러 수정

--- chelak/src/main/java/chela/kotlin/regex/reV.kt
+++ chelak/src/main/java/chela/kotlin/regex/reV.kt
@@ -39,8 +39,8 @@ object reV: ChRegex(
             else->v.toLong()
         }
     }
-    fun num(it:String):Number? = reV.match(it)?.let{
+    fun num(it:String):Number? = if(re.matches(it)) reV.match(it)?.let{
         it.groups[3]?.let{reV.group3(it)} ?:
         it.groups[4]?.let{reV.group4(it)}
-    }
+    } as? Number? else null
 }
